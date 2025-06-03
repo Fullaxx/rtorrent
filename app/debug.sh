be295ac5abe0f595d2e04b90187ff890cb80600a
Brett Kuskie <fullaxx@gmail.com>
2025-06-03 16:50:24 +0000

    Add newlines to app/debug.sh

--- app/debug.sh
+++ app/debug.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 ip addr show
+echo
+
 ip route show
+echo
 
 echo -n "www.icanhazip.com: "
 curl https://www.icanhazip.com
+echo
 
 echo -n "ifconfig.me: "
 curl https://ifconfig.me
